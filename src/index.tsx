f348011d50f565361ea46d76d772cad00f7c75e6
Sumi <qkobr94@gmail.com>
2023-01-05 02:17:10 +0900

    feat(esBuild): copy esbuild into public file

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,27 @@
+import * as esbuild from "esbuild-wasm";
+import { useState } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <h1>hi</h1>;
+  const [input, setInput] = useState<string>("");
+  const [code, setCode] = useState<string>("");
+
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
