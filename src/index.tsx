d96efe63fbbc25ea6a54919a8b4319dbaf485f32
Sumi <qkobr94@gmail.com>
2023-01-06 21:43:43 +0900

    feat(esBuild): initialize esbuild services

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
 import * as esbuild from "esbuild-wasm";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
   const [input, setInput] = useState<string>("");
   const [code, setCode] = useState<string>("");
+  useEffect(() => {
+    startService();
+  }, []);
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+    console.log(service);
+  };
 
   const onClick = () => {
     console.log(input);
