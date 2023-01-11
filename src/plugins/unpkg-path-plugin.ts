52508f2944dfb1f03a8faaf378fc746b24eea08c
Sumi <qkobr94@gmail.com>
2023-01-12 04:48:35 +0900

    feat(localForage): save caches in indexedDB and fix type error

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,10 @@
 import * as esbuild from "esbuild-wasm";
 import axios from "axios";
+import localForage from "localforage";
+
+const fileCache = localForage.createInstance({
+  name: "filecache",
+});
 
 export const unpkgPathPlugin = () => {
   return {
@@ -29,7 +34,6 @@ export const unpkgPathPlugin = () => {
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log("onLoad", args);
-
         if (args.path === "index.js") {
           return {
             loader: "jsx",
@@ -40,13 +44,27 @@ export const unpkgPathPlugin = () => {
             `,
           };
         }
+        //Check to see if we have already fetched this file
+
+        //and if it is in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+        //if it is, return it immediately
+        if (cachedResult) {
+          return cachedResult;
+        }
+
         const { data, request } = await axios.get(args.path);
-        console.log(request);
-        return {
+
+        const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
+        //store response in cache
+        await fileCache.setItem(args.path, result);
+        return result;
       });
     },
   };
