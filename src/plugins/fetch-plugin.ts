6298498fbcbb73da78e0696eabae03f3dea5261f
Sumi <qkobr94@gmail.com>
2023-01-12 23:35:45 +0900

    feat(plugin): refactor onLoad function

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,13 +10,45 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: inputCode,
-          };
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+        //if it is, return it immediately
+        if (cachedResult) {
+          return cachedResult;
         }
+
+        const { data, request } = await axios.get(args.path);
+        const escaped = data
+          .replace(/\n/g, "")
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+
+        const contents = `
+          const style = document.createElement('style');
+          style.innerText = '${escaped}';
+          document.head.appendChild(style);
+          `;
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+        //store response in cache
+        await fileCache.setItem(args.path, result);
+        return result;
+      });
+
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: "jsx",
+          contents: inputCode,
+        };
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         //Check to see if we have already fetched this file
 
         //and if it is in the cache
@@ -29,23 +61,10 @@ export const fetchPlugin = (inputCode: string) => {
         }
 
         const { data, request } = await axios.get(args.path);
-        const fileType = args.path.match(/.css$/) ? "css" : "jsx";
-        const escaped = data
-          .replace(/\n/g, "")
-          .replace(/"/g, '\\"')
-          .replace(/'/g, "\\'");
 
-        const contents =
-          fileType === "css"
-            ? `
-        const style = document.createElement('style');
-        style.innerText = '${escaped}';
-        document.head.appendChild(style);
-        `
-            : data;
         const result: esbuild.OnLoadResult = {
           loader: "jsx",
-          contents,
+          contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
         //store response in cache
