8007805284ec4f8ef5f0afd476cd340557fbaead
Sumi <qkobr94@gmail.com>
2023-01-25 15:44:31 +0100

    feat(iframe): eval code inside of iframe

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState<string>("");
   const [code, setCode] = useState<string>("");
 
@@ -37,14 +38,24 @@ const App = () => {
       },
     });
 
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
 
   const html = `
-  <script>
-  ${code}
-  </script>
+   <html>
+    <head></head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener("message", (event) => {
+          eval(event.data);
+        },false)
+      </script>
+    </body>
+   </html>
   `;
+
   return (
     <div>
       <textarea
@@ -55,7 +66,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe sandbox="allow-scripts" srcDoc={html}></iframe>
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html}></iframe>
     </div>
   );
 };
