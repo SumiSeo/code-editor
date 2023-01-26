c39f0e23ca4a9cf7229e8fafa7cfb4840b92528f
Sumi <qkobr94@gmail.com>
2023-01-26 15:30:49 +0100

    feat(monacoEditor): add monaco-editor options

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -3,7 +3,18 @@ import MonacoEditor from "@monaco-editor/react";
 const CodeEditor = () => {
   return (
     <MonacoEditor
-      options={{ wordWrap: "on" }}
+      options={{
+        wordWrap: "on",
+        minimap: {
+          enabled: false,
+        },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
       language="javascript"
       height="500px"
       theme="dark"
