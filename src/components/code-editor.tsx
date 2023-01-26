87c9b5a8ef59ed934fcb486ca15e37ab2f941cd5
Sumi <qkobr94@gmail.com>
2023-01-26 15:26:27 +0100

    feat(monacoEditor): add wordwrap property

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,6 +1,13 @@
 import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor = () => {
-  return <MonacoEditor language="javascript" height="500px" theme="dark" />;
+  return (
+    <MonacoEditor
+      options={{ wordWrap: "on" }}
+      language="javascript"
+      height="500px"
+      theme="dark"
+    />
+  );
 };
 export default CodeEditor;
