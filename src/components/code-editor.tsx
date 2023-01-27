a9f957cfc45f97069119e4a609fe332755748b68
Sumi <qkobr94@gmail.com>
2023-01-27 14:39:28 +0100

    feat(monacoEditor): connect monaco editor to input value

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,39 +1,48 @@
-import MonacoEditor from "@monaco-editor/react";
+import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
+
 interface CodeEditorProps {
   initialValue: string;
   onChange: (value: string) => void;
 }
 
-const CodeEditor = ({
-  initialValue,
-  onChange,
-}: CodeEditorProps): JSX.Element => {
-  const onEditorDidMount = (getValue: () => string, MonacoEditor: any) => {
-    MonacoEditor.onDidChangeModelContent(() => {
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    monacoEditor.onDidChangeModelContent(() => {
       onChange(getValue());
     });
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
   };
 
+  const onFormatClick = () => {
+    //get current value
+    //format
+    //set the formatte value back in the editor
+  };
   return (
-    <MonacoEditor
-      editorDidMount={onEditorDidMount}
-      value={initialValue}
-      options={{
-        wordWrap: "on",
-        minimap: {
-          enabled: false,
-        },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-      }}
-      language="javascript"
-      height="500px"
-      theme="dark"
-    />
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      <MonacoEditor
+        editorDidMount={onEditorDidMount}
+        value={initialValue}
+        options={{
+          wordWrap: "on",
+          minimap: {
+            enabled: false,
+          },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+        language="javascript"
+        height="500px"
+        theme="dark"
+      />
+    </div>
   );
 };
 export default CodeEditor;
