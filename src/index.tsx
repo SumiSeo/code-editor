4a250e0fbaf19524104892f41d561b80336b76a3
Sumi <qkobr94@gmail.com>
2023-01-31 17:15:53 +0100

    style: isolate bundler and arrange folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,17 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
-import * as esbuild from "esbuild-wasm";
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-import { fetchPlugin } from "./plugins/fetch-plugin";
 import CodeEditor from "./components/code-editor";
 import Preview from "./components/preview";
+import bundle from "./bundler";
 
 const App = () => {
-  const ref = useRef<any>();
   const [code, setCode] = useState<string>("");
   const [input, setInput] = useState<string>("");
 
-  useEffect(() => {
-    startService();
-  }, []);
-
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
-    });
-  };
-
   const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    const result = await ref.current.build({
-      entryPoints: ["index.js"],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        "process.env.NODE_ENV": '"production"',
-        global: "window",
-      },
-    });
-    setCode(result.outputFiles[0].text);
+    const output = await bundle(input);
+    const result = setCode(output);
   };
 
   return (
@@ -47,10 +20,7 @@ const App = () => {
         initialValue="const a = 3"
         onChange={(value) => setInput(value)}
       />
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
+
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
