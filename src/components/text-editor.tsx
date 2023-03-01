fb2de48039383f07eebf9552a0695f9b8c80f03e
Sumi <equepem@gmail.com>
2023-03-01 15:31:42 +0100

    feat(markdownEditor): create markdown event and add click event for toggling

---
+++ src/components/text-editor.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect, useRef } from "react";
+import MDEditor from "@uiw/react-md-editor";
+
+const TextEditor: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        console.log("element clicked on is inside editor");
+        return;
+      }
+      console.log("element clicked is not inside editor");
+      setEditing(false);
+    };
+    document.addEventListener("click", listener, { capture: true });
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
+  const [editing, setEditing] = useState<boolean>(false);
+
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor />
+      </div>
+    );
+  }
+
+  return (
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={"# Header"} />
+    </div>
+  );
+};
+
+export default TextEditor;
