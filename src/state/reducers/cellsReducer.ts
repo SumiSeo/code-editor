b9a0ddafc170e76a1bf3c3730c6519db2d5063b8
Sumi <equepem@gmail.com>
2023-03-03 11:38:00 +0100

    chore: refactor redundnat codes

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -24,13 +24,14 @@ const cellsReducer = (
 ): CellsState => {
   switch (action.type) {
     case ActionType.UPDATE_CELL:
+      const { id, content } = action.payload;
       return {
         ...state,
         data: {
           ...state.data,
-          [action.payload.id]: {
-            ...state.data[action.payload.id],
-            content: action.payload.content,
+          [id]: {
+            ...state.data[id],
+            content,
           },
         },
       };
